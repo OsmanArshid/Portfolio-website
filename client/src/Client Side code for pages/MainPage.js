8978a652212308036f3a73ef49e34536d017c196
Muhammad Mehdi Changezi <113912243+Muhammad-Mehdi-Changazi@users.noreply.github.com>
2024-10-28 01:59:43 +0500

    Updated Url

--- client/src/Client Side code for pages/MainPage.js
+++ client/src/Client Side code for pages/MainPage.js
@@ -41,7 +41,7 @@ const MainPage = () => {
         e.preventDefault();
 
         // Send data to the backend
-        fetch('portfolio-website-five-ebon.vercel.app/send', {
+        fetch('http://portfolio-website-ruby-one.vercel.app/send', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
